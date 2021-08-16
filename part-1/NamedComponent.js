323bed7ccfdc1aa94a2cf38b09ac3507964d2ceb
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2021-08-16 16:55:35 -0700

    finish

--- part-1/NamedComponent.js
+++ part-1/NamedComponent.js
@@ -1,4 +1,4 @@
-function NamedComponent() {
+function NamedComponent(props) {
   // renders a p that should accept a property of “name” and display text “My name is name”.
   return <p>My name is {props.name}.</p>;
 }
